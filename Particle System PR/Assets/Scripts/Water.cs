6f51cbf7eacbaf63e5b6e557479a521f62a6e1bd
Максим Майнагашев <maxuser99@mail.ru>
2022-07-30 01:38:47 +0700

    Water splash added

---
+++ Particle System PR/Assets/Scripts/Water.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+     [SerializeField] private GameObject _effect;
+
+     private void OnCollisionEnter2D(Collision2D col)
+     {
+          Physics2D.IgnoreCollision(col.collider, col.otherCollider);
+
+          Instantiate(_effect, col.GetContact(0).point, Quaternion.identity);
+     }
+}
